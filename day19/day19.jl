57cde1411a60708ee67e6495e1af4943b896b671
Ilan <ilan.pillemer@gmail.com>
2021-12-25 23:29:54 +0000

    work in progress

--- day19/day19.jl
+++ day19/day19.jl
@@ -5,38 +5,42 @@ include("example.jl")
 origin = exScans[1]
 
 function test(left, right)
-    a = Set([])
-    b = Set([])
-    c = Set([])
+    found = false
+    i = 1
+    while i < length(left) / 3
+        a = Set([])
+        b = Set([])
+        c = Set([])
 
-    for p1 in eachrow(left)
-        for p2 in eachrow(left)
-            push!(a, p1 - p2)
-            #push!(a, p1 - left[1, :])
+        for p1 in eachrow(left)
+            for p2 in eachrow(left)
+                #push!(a, p1 - p2)
+                push!(a, p1 - left[i, :])
+            end
         end
-    end
 
-    for p1 in eachrow(right)
-        for p2 in eachrow(right)
-            push!(b, p1 - p2)
-            #push!(b, p1 - right[1, :])
+        for p1 in eachrow(right)
+            for p2 in eachrow(right)
+                push!(b, p1 - p2)
+                #push!(b, p1 - right[1, :])
+            end
         end
-    end
-    for (i, r) in enumerate(rall)
-        c = [r * z for z in b]
-        result = intersect(a, c)
-        #println(length(result))
-        if length(result) == 133
+        for (i, r) in enumerate(rall)
+            c = [r * z for z in b]
+            result = intersect(a, c)
             #println(length(result))
-            return (r,true)
+            if length(result) == 12
+                #println(length(result))
+                return (r, true)
+            end
         end
+        i = i + 1
     end
-    rall[1],false
+    (rall[1], false)
 end
 
 
 function checkRotation(origin, point)
-
     found = 0
     for row = 1:div(length(origin), 3)
         if point == origin[row, :]
@@ -47,32 +51,31 @@ function checkRotation(origin, point)
     found
 end
 
-
-function test2()
-    for scan = 2:length(exScans)
-        for (i, rot) in enumerate(rall)
-            #println("rot", i)
-            found = 0
-            for p in eachrow(exScans[scan])
-                #println("...",rot*p,typeof(rot*p))
-                x1 = rot * p
-                x1 = x1 + [68, -1246, -43]
-                found = found + checkRotation(origin, x1)
-            end
-            if found != 0
-                println("found $found matches at rotation $i for scanner $scan")
-            end
+function getNext(scan, scans)
+    #find link from one scanner to next scanner
+    n = length(scans)
+    for i = 1:length(scans)
+        (rot, ok) = test(scan, scans[i])
+        if ok
+            #println("scanner $i is next scanner")
+            return rot, i
         end
     end
 end
 
-function getSequence() 
-
-
+function getSequence(scans)
+    for (i, scan) in enumerate(scans)
+        (rot, j) = getNext(scan, scans)
+        println("Scanner $i links to Scanner $j with rotation $rot")
+    end
 end
- 
 
-println(test(origin, exScans[2]))
-println(test(exScans[2], exScans[1]))
-println(test(exScans[2], exScans[3]))
-println(test(exScans[2], exScans[4]))
+
+#println(test(origin, exScans[2]))
+#println(test(exScans[2], exScans[1]))
+#println(test(exScans[2], exScans[3]))
+#println(test(exScans[2], exScans[4]))
+#println(test(exScans[2], exScans[5]))
+
+#getNext(origin, exScans)
+getSequence(exScans)

277d8224549d743583aa1e332d9dd00cf84e603e
Ilan <ilan.pillemer@gmail.com>
2021-12-25 20:51:13 +0000

    work in progress

--- day19/day19.jl
+++ day19/day19.jl
@@ -4,15 +4,35 @@ include("example.jl")
 
 origin = exScans[1]
 
+function test()
+    a = Set([])
+    b = Set([])
+
+    for p1 in eachrow(origin)
+        for p2 in eachrow(origin)
+            push!(a, p1 - p2)
+        end
+    end
+
+    for p1 in eachrow(exScans[2])
+        for p2 in eachrow(exScans[2])
+            push!(b, p1 - p2)
+        end
+    end
+    for (i,r) in enumerate(rall)
+        c = [r * z for z in b]
+        intersect(a, c)
+        println(length(intersect(a, c)))
+    end
+end
+
 
 function checkRotation(origin, point)
+    
     found = 0
     for row = 1:div(length(origin), 3)
         if point == origin[row, :]
             found = 1
-            #println("scanner", scan)
-            #println("translated", x1)
-            #println("rotated", rot * p)
             println("scanned", point)
         end
     end
@@ -22,7 +42,7 @@ end
 
 for scan = 2:length(exScans)
     for (i, rot) in enumerate(rall)
-        println("rot", i)
+        #println("rot", i)
         found = 0
         for p in eachrow(exScans[scan])
             #println("...",rot*p,typeof(rot*p))
@@ -31,7 +51,9 @@ for scan = 2:length(exScans)
             found = found + checkRotation(origin, x1)
         end
         if found != 0
-            println("found $found matches at rotation $i")
+            println("found $found matches at rotation $i for scanner $scan")
         end
     end
 end
+
+test()

e3c27e4baff026314bf67842349f5d8bac7ca2a1
Ilan <ilan.pillemer@gmail.com>
2021-12-31 17:33:33 +0000

    d21 part2

--- day21/day21.jl
+++ day21/day21.jl
@@ -45,3 +45,33 @@ function play(p1, p2)
     println("losing score is: $(min(score1,score2))")
     println("$((min(score1,score2)) * rolls)")
 end
+
+memo = Dict()
+function play2(p1, p2, s1, s2)
+    if s1 >= 21
+        return [1, 0]
+    end
+    if s2 >= 21
+        return [0, 1]
+    end
+    if haskey(memo, (p1, p2, s1, s2))
+        return memo[(p1, p2, s1, s2)]
+    end
+    total = [0, 0]
+
+    for i = 1:3, j = 1:3, k = 1:3
+        next_p1 = (p1 + i + j + k) % 10
+        if next_p1 == 0
+            next_p1 = 10
+        end
+        local next_s1 = s1 + next_p1
+        local score = play2(p2, next_p1, s2, next_s1)
+        total = [total[1] + score[2], total[2] + score[1]]
+    end
+    global memo[(p1, p2, s1, s2)] = total
+    return total
+end
+#Player 1 starting position: 4
+#Player 2 starting position: 3
+result = play2(4, 3, 0, 0)
+max(result[1], result[2])

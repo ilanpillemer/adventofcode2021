aead0cc965d21f814e3a099541da08e107f6e306
Ilan <ilan.pillemer@gmail.com>
2022-01-02 22:53:53 +0000

    d22 minor speed impr

--- day22/day22.jl
+++ day22/day22.jl
@@ -1,17 +1,17 @@
 println("day22")
 
-X = []
-Y = []
-Z = []
+X = Int[]
+Y = Int[]
+Z = Int[]
 steps = []
 
 struct Step
-    x::Any
-    x1::Any
-    y::Any
-    y1::Any
-    z::Any
-    z1::Any
+    x::Int
+    x1::Int
+    y::Int
+    y1::Int
+    z::Int
+    z1::Int
     state::Any
 end
 
@@ -64,25 +64,28 @@ function part2(X, Y, Z)
     grid = zeros(Bool, N, N, N)
 
     allsteps = length(steps)
-    for (i, s) in enumerate(steps)
+    i = 0
+    for s in steps
+        i = i + 1
         #println("processing step $i of $allsteps")
-        x = findfirst(x -> x == s.x, X)
-        x1 = findfirst(x -> x == s.x1, X)
-        y = findfirst(x -> x == s.y, Y)
-        y1 = findfirst(x -> x == s.y1, Y)
-        z = findfirst(x -> x == s.z, Z)
-        z1 = findfirst(x -> x == s.z1, Z)
-        for i = x:x1-1, j = y:y1-1, k = z:z1-1
-            grid[i, j, k] = (s.state == "on")
+        x = searchsortedfirst(X, s.x)
+        x1 = searchsortedfirst(X, s.x1)
+        y = searchsortedfirst(Y, s.y)
+        y1 = searchsortedfirst(Y, s.y1)
+        z = searchsortedfirst(Z, s.z)
+        z1 = searchsortedfirst(Z, s.z1)
+        for x = x:x1-1, y = y:y1-1, z = z:z1-1
+            grid[x, y, z] = (s.state == "on")
         end
     end
+    #println(grid)
     total = 0
-    for x = 1:N-1, y = 1:N-1, z = 1:N-1
-        process = grid[x, y, z]
-        if process
-            cuboid_volume = ((X[x+1] - X[x]) * (Y[y+1] - Y[y]) * (Z[z+1] - Z[z]))
-            total = total + cuboid_volume
-        end
+
+
+
+    @fastmath @inbounds for x = 1:N-1, y = 1:N-1, z = 1:N-1
+
+        total += grid[x, y, z] * ((X[x+1] - X[x]) * (Y[y+1] - Y[y]) * (Z[z+1] - Z[z]))
     end
     total
 end
